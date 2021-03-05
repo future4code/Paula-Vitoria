22f5efb093cf881f4b3ef7c6f08e3fd44a01da07
Paula Lopes <paulalopessantos28@gmail.com>
2021-03-05 17:08:25 -0300

    Lista de exercicios js

---
+++ semana4/lista-de-exercicios/lista-js-boilerplate/lista-js-boilerplate/exercicios.js
@@ -0,0 +1,250 @@
+//Exercício 1
+function inverteArray(array) {
+  //retornar o PRÓPRIO ARRAY INVERTIDO!
+     
+}
+
+//Exercício 2
+
+function retornaNumerosParesElevadosADois (array) {
+   let novoArray =[];
+  for(numero of array){
+     if(numero%2===0){
+        novoArray.push(numero*numero);
+      }
+}
+return novoArray;
+}
+
+
+//Exercício 3
+
+function retornaNumerosPares (array) {
+   let novoArray =[]
+   for(numero of array){
+      if(numero%2===0){
+         novoArray.push(numero);
+
+      }
+   }
+  return novoArray;
+}
+
+
+//Exercício 4
+
+function retornaMaiorNumero(array) {
+   let maior = array[0];
+   for(let i =0; i<array.length; i++){
+      if(array[i]> maior){
+         maior = array[i];
+      }
+   }
+   return maior;
+}
+
+
+//Exercício 5
+
+function retornaQuantidadeElementos (array) {
+   let quantidade = 0;
+   for(elemento of array){
+      quantidade++;
+   }
+   return quantidade;
+}
+
+//Exercício 6
+
+function retornaExpressoesBooleanas() {
+
+}
+
+
+//Exercício 7
+
+function retornaNNumerosPares(n) {
+  
+   
+}
+
+
+
+// Exercício 8
+
+function checaTriangulo(a, b, c) {
+   
+   if(a===b && a===c && c===b){
+      return 'Equilátero';
+      
+   }else if(a!==b && a!==c && b!==c){
+       return'Escaleno';
+
+   }else{
+       return'Isósceles';
+   }
+}
+
+
+
+// Exercício 9
+
+function comparaDoisNumeros(num1, num2) {
+ /*  let menor;
+   let obj = {
+      maiorNumero:0,
+      maiorDivisivelporMenor:true,
+      diferenca:0,
+   }
+
+   if(num1 > num2){
+      obj.maiorNumero = num1;
+      menor=num2;
+   }else{
+      obj.maiorNumero = num2;
+      menor= num1;
+   }
+
+   console.log(obj.maiorNumero);*/
+   
+}
+
+comparaDoisNumeros(2,4);
+// Exercício 10
+
+function segundoMaiorEMenor(array) {
+  /* let maiorNumero=array[0];
+   let menorNumero=array[0];
+   let novoArray =[];
+*/
+
+
+}
+
+//Exercício 11
+
+function ordenaArray(array) {
+
+   
+}
+
+// Exercício 12
+
+function filmeFavorito() {
+   const filmeAstrodev = {
+      nome:"O diabo Veste Prada",
+      ano: 2006,
+      diretor: "David Frankel",
+      atores:["Meryl Streep", "Ane Hathaway","Emily Blunt","Stanley Tucci"],
+
+   }
+}
+
+// Exercício 13
+
+function imprimeChamada() {
+   // implemente sua lógica aqui
+}
+
+// Exercício 14
+
+function criaRetangulo(lado1, lado2) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 15
+
+function anonimizaPessoa(pessoa) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 16
+
+const arrayDePessoas = [
+  { nome: "Pedro", idade: 20 },
+  { nome: "João", idade: 10 },
+  { nome: "Paula", idade: 12 },
+  { nome: "Artur", idade: 89 } 
+]
+
+// Exercício 16, letra A
+
+function maioresDe18(arrayDePessoas) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 16, letra B
+
+function menoresDe18(arrayDePessoas) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 17, letra A
+
+function multiplicaArrayPor2(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 17, letra B
+
+function multiplicaArrayPor2S(array) {
+  // implemente sua lógica aqui
+}
+
+// Exercício 17, letra C
+
+function verificaParidade(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 18
+
+const pessoas = [
+  { nome: "Paula", idade: 12, altura: 1.8},
+  { nome: "João", idade: 20, altura: 1.3},
+  { nome: "Pedro", idade: 15, altura: 1.9},
+  { nome: "Luciano", idade: 22, altura: 1.8},
+  { nome: "Artur", idade: 10, altura: 1.2},
+  { nome: "Soter", idade: 70, altura: 1.9}
+]
+
+//Exercício 18, letra A
+
+function retornaPessoasAutorizadas(pessoas) {
+   // implemente sua lógica aqui
+}
+
+
+// Exercício 18, letra B
+
+function retornaPessoasNaoAutorizadas(pessoas) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 19
+
+const consultas = [
+  { nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
+  { nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
+  { nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
+  { nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
+  ]
+
+function retornaEmailConsulta(consultas) {
+  // implemente sua lógica aqui
+}
+
+//Exercício 20
+
+const contas = [
+  { cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+  { cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+  { cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+  { cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+  { cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+  { cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+function atualizaSaldo() {
+  // implemente sua lógica aqui
+}
