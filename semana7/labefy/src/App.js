713be7ad9a5268816968fd8d55ae199193b7a2a2
Paula Lopes <paulalopessantos28@gmail.com>
2021-03-28 21:53:08 -0300

    ultimas alteracoes labefy

--- semana7/labefy/src/App.js
+++ semana7/labefy/src/App.js
@@ -2,15 +2,51 @@ import React from "react"
 import PlayListPage from "./components/PlayListPage"
 import CreatePlayListPage from "./components/CreatePlayListPage"
 
-function App() {
-  return (
-    <div className="App">
-      <CreatePlayListPage/>
-      <PlayListPage/>
+
+
+
+export default class App extends React.Component {
+  state = {
+    page:'createPlaylistPage'
+  }
+
+  changeToCreatePlaylistPage = () =>{
+    if(this.state.page === 'playlistPage'){
+      this.setState({page:'createPlaylistPage'});
+    }
+  }
+
+  changeToPlayListPage = () =>{
+    if(this.state.page === 'createPlaylistPage'){
+      this.setState({page:'playlistPage'});
+    }
+  }
+  renderRightPage = () =>{
+    if (this.state.page === 'createPlaylistPage' ){
+       return <CreatePlayListPage
+        changeToPlayListPage={this.changeToPlayListPage}/>
+    }
+      else if (this.state.page === 'playlistPage'){
+      return <PlayListPage
+      changeToCreatePlaylistPage={this.changeToCreatePlaylistPage} />
+      }else{
+        return<div></div>
+      }
+        
+      }
+  
+  render(){ 
+
+
+    
+    return (
+    <div>
+      {this.renderRightPage()}
+  
       
-     
     </div>
   );
 }
+}
 
-export default App;
+
