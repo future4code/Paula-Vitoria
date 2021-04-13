7397fb6f4234de33cd186558759d57be700c8dba
Paula Lopes <paulalopessantos28@gmail.com>
2021-04-13 16:51:21 -0300

    Adição das rotas

---
+++ semana10/labex/src/pages/ListTripsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListTripsPage = () => {
+  return <div>ListTripsPage</div>;
+};
+
+export default ListTripsPage;
---
+++ semana10/labex/src/routes/Router.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AdminHomePage from "../pages/AdminHomePage";
+import ApplicationFormPage from "../pages/ApplicationFormPage";
+import CreateTripPage from "../pages/CreateTripPage";
+import HomePage from "../pages/HomePage";
+import ListTripsPage from "../pages/ListTripsPage";
+import LoginPage from "../pages/LoginPage";
+import TripDetailsPage from "../pages/TripDetailsPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+
+        <Route exact path="/trips/list">
+          <ListTripsPage />
+        </Route>
+
+        <Route exact path="/trips/application">
+          <ApplicationFormPage />
+        </Route>
+
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+
+        <Route exact path="/admin/trips/list">
+          <AdminHomePage />
+        </Route>
+
+        <Route exact path="/admin/trips/create">
+          <CreateTripPage />
+        </Route>
+
+        <Route exact path="/admin/trips/:id">
+          <TripsDetailPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
