4977d47971b5fa81815309445b87323cdf2e0bde
paulinhalopessantos <linha_vitoria@hotmail.com>
2021-05-19 23:40:59 -0300

    endpoints alterações

--- semana15/aula43/src/index.ts
+++ semana15/aula43/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
-import { countries } from "./countries";
+import { countries, country } from "./countries";
 
 const app: Express = express();
 app.use(express.json());
@@ -18,20 +18,61 @@ app.get("/countries/all", (req: Request, res: Response) => {
   res.send(result);
 });
 
-//app.get("/countries/")
-
 app.get("/countries/search", (req: Request, res: Response) => {
   const name = req.query.name as string;
   const capital = req.query.capital as string;
   const continent = req.query.continent as string;
-  
+
+  let result: country[] = countries;
+
+  if (name) {
+    result = result.filter((country) => country.name.includes(name));
+  }
+  if (capital) {
+    result = result.filter((country) => country.name.includes(capital));
+  }
+  if (continent) {
+    result = result.filter((country) => country.continent.includes(continent));
+  }
+
+  if (result.length) {
+    res.status(200);
+    res.send(result);
+  } else {
+    res.status(404).send("Not Found");
+  }
 });
 
-app.get("/countries/:id", (req: Request, res: Response) => {
+app.put("/countries/edit/:id", (req: Request, res: Response) => {
   const id = Number(req.params.id);
-  const result = countries.find((country) => country.id === id);
-  res.send(result);
-  console.log(result);
+  const name = req.body.name;
+  const capital = req.body.capital;
+
+  countries.map((country) => {
+    if (country.id === id) {
+      country.name = name;
+      country.capital = capital;
+    }
+  });
+  res.status(200);
+  res.send("Deu certo!");
+});
+app.get("/countries/:id", (req: Request, res: Response) => {
+  try {
+    if (isNaN(Number(req.params.id))) {
+      throw new Error("Id must be a number");
+    }
+    const id = Number(req.params.id);
+    const result = countries.find((country) => country.id === id);
+    if (!result) {
+      throw new Error("Could not country with specified id");
+    }
+    res.status(200);
+    res.send(result);
+    console.log(result);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
 });
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
