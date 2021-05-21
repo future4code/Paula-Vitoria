c00d57c7264d3af694bb4e95e5ad499c21d40a7f
paulinhalopessantos <linha_vitoria@hotmail.com>
2021-05-20 22:35:25 -0300

    incluindo novos endpoints

--- semana15/aula44/src/index.ts
+++ semana15/aula44/src/index.ts
@@ -1,83 +1,38 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-
-type User = {
-  id: number;
-  name: string;
-  email: string;
-  type: string;
-  age: number;
-};
-
-// Mock simulando um array de usuários no Banco de Dados
-let users: User[] = [
-  {
-    id: 1,
-    name: "Alice",
-    email: "alice@email.com",
-    type: "ADMIN",
-    age: 12,
-  },
-  {
-    id: 2,
-    name: "Bob",
-    email: "bob@email.com",
-    type: "NORMAL",
-    age: 36,
-  },
-  {
-    id: 3,
-    name: "Coragem",
-    email: "coragem@email.com",
-    type: "NORMAL",
-    age: 21,
-  },
-  {
-    id: 4,
-    name: "Dory",
-    email: "dory@email.com",
-    type: "NORMAL",
-    age: 17,
-  },
-  {
-    id: 5,
-    name: "Elsa",
-    email: "elsa@email.com",
-    type: "ADMIN",
-    age: 17,
-  },
-  {
-    id: 6,
-    name: "Fred",
-    email: "fred@email.com",
-    type: "ADMIN",
-    age: 60,
-  },
-];
+import { users } from "./users";
+//import {type} from "./users"
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Atividade 1
+//a)O método HTTP que devo utilizar para fazer esse tipo de requisição é o método get.
+//b)eu indicaria essa entidade como "users/all"
 app.get("/users/all", (req: Request, res: Response) => {
   res.send(users);
-  //a)O método HTTP que devo utilizar para fazer esse tipo de requisição é o método get.
-  //b)eu indicaria essa entidade como "users/all"
 });
+//Atividade 2
+//a) Passei como path params, pois minha intenção é filtrar os valores por type
+//b)Sim, utilizando um Enum e fazendo a validação do valor recebido, garantindo q ele esteja presente no
+//objeto que representa esse Enum.
 
-app.get("/users", (req: Request, res: Response) => {
+app.get("/users/:type", (req: Request, res: Response) => {
   try {
-    const query = req.query;
-    console.log(query);
-    res.status(200).send();
+    const type = req.params.type;
+
+    const result = users.filter((user) => {
+      return user.type.toLowerCase() === type;
+    });
+
+    res.status(200).send(result);
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
 });
-// Para testar se o servidor está tratando os endpoints corretamente
-app.get("/ping", (req: Request, res: Response) => {
-  res.status(200).send("pong!");
-});
+
+//Atividade 3;
 
 app.listen(3003, () => {
   console.log("Server is running at port 3003");
