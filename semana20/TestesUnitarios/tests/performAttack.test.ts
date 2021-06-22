776dd9db3a1da5495c33f3a9ca81e480ab729c0e
paula-lopes <paulalopessantos28@gmail.com>
2021-06-22 19:52:26 -0300

    exercise 6 completed

--- semana20/TestesUnitarios/tests/performAttack.test.ts
+++ semana20/TestesUnitarios/tests/performAttack.test.ts
@@ -53,4 +53,107 @@ describe("Testing our function performAttack",()=>{
         expect(validatorMock).toHaveBeenCalled()
         }
     })
+
+
+    test("Error when defenser of defender is majon than attacker force",()=>{
+
+        const validatorMock = jest.fn(()=>{
+            return true;
+        })
+        const attacker:Character ={
+            name:"Ryu",
+            life:1500,
+            defense:600,
+            force:200
+        }
+         const defender:Character ={
+            name:"Cammy",
+            life:1500,
+            defense:600,
+            force: 500
+        }
+
+        
+        performAttack(attacker, defender,validatorMock)
+        expect(defender.defense).toBeGreaterThanOrEqual(attacker.force)
+       
+    })
+
+
+    test("Error when the name's attacker is a empty string ",()=>{
+
+        const validatorMock = jest.fn(()=>{
+            return false;
+        })
+        const attacker:Character ={
+            name:"",
+            life:1500,
+            defense:600,
+            force:200
+        }
+         const defender:Character ={
+            name:"Cammy",
+            life:1500,
+            defense:600,
+            force: 500
+        }
+
+        try{
+        performAttack(attacker, defender,validatorMock)
+        }catch(err){
+        expect(err.message).toBe("Invalid Character")
+        }
+    })
+
+        
+    test("Error when defense of defender is equals force ",()=>{
+
+        const validatorMock = jest.fn(()=>{
+            return false;
+        })
+        const attacker:Character ={
+            name:"Ryu",
+            life:1500,
+            defense:600,
+            force:600
+        }
+         const defender:Character ={
+            name:"Cammy",
+            life:1500,
+            defense:600,
+            force: 500
+        }
+
+     
+        performAttack(attacker, defender,validatorMock)
+        expect(defender.defense).toEqual(attacker.force)
+      
+    })
+    test("Error when force less than zero ",()=>{
+
+        const validatorMock = jest.fn(()=>{
+            return false;
+        })
+        const attacker:Character ={
+            name:"Ryu",
+            life:1500,
+            defense:600,
+            force:600
+        }
+         const defender:Character ={
+            name:"Cammy",
+            life:1500,
+            defense:600,
+            force: -5
+        }
+
+     try{
+        performAttack(attacker, defender,validatorMock)
+     }catch(err){
+        expect(err.message).toEqual("Invalid Character")
+     }
+    })
+
+
+
 })
